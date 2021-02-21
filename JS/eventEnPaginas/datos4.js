e31c9d55c239c84d7aa6e4539eb89360bf3da569
luisangeluis <luisangeluis@gmail.com>
2021-02-21 15:49:11 -0600

    Se posiciona footer, se acomodan metodos footer y panel de detalles credito dinamico

--- JS/eventEnPaginas/datos4.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    const btnSlideInvert = document.querySelectorAll('.btn-slide-invert');
-
-    $(btnSlideInvert).on({
-        click:function(){
-            expandirPanelInvert(btnSlideInvert,400);
-        }
-    })
-});
--- JS/eventEnPaginas/datos4A.js
+++ JS/eventEnPaginas/datos4A.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     const btnSlideInvert=document.querySelectorAll('.btn-slide-invert');
+    //AJUSTAR FOOTER HASTA ABAJO
+    ajustarFooter();
+    //EVENTO PARA AJUSTAR FOOTER
+    $(window).on({
+        resize: function () {
 
+            ajustarFooter();
+        }
+    })
+    //PANEL INVERTIDO
     for(let x=0; x<btnSlideInvert.length;x++){
         $(btnSlideInvert).on({
             click: function(){

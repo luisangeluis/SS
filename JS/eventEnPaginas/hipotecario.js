3f313f6f496a456b82752275f45eddc2cd780c03
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2021-03-24 10:04:01 -0600

    Tabla de amortización

--- JS/eventEnPaginas/hipotecario.js
+++ JS/eventEnPaginas/hipotecario.js
@@ -4,99 +4,31 @@ const tBody = document.querySelector('#amortizacion tbody');
 
 getTablaAmortizacion(saldoTotalTabla, numDePeriodos, tBody);
 
-rangeEngancheFinanciamiento.addEventListener('mouseup', () => {
-
-    let elementosArray = Array.from(tBody.children);
-    console.log(elementosArray);
-
-    for (let x = 0; x < elementosArray.length; x++) {
-        tBody.removeChild(elementosArray[x]);
-    }
+rangeEngancheFinanciamiento.addEventListener('change', () => {
 
     saldoTotalTabla = valorFinanciamiento.value.replace('$', '');
-    getTablaAmortizacion(saldoTotalTabla, numDePeriodos, tBody);
-
-});
-
-rangeAnos.addEventListener('mouseup', () => {
 
     clearTablaAmortizacion(tBody);
-    /*
-    let elementosArray = Array.from(tBody.children);
-    console.log(elementosArray);
+    getTablaAmortizacion(saldoTotalTabla, numDePeriodos, tBody);
 
-    for (let x = 0; x < elementosArray.length; x++) {
-        tBody.removeChild(elementosArray[x]);
-    }
+});
+
+rangeAnos.addEventListener('change', () => {
+    /*
+        let elementosArray = Array.from(tBody.children);
+        console.log(elementosArray);
+    
+        for (let x = 0; x < elementosArray.length; x++) {
+            tBody.removeChild(elementosArray[x]);
+        }
     */
+    clearTablaAmortizacion(tBody);
     getTablaAmortizacion(saldoTotalTabla, numDePeriodos, tBody);
 
 
 });
 
-function clearTablaAmortizacion(pElementoHijos){
-    let elementosArray = Array.from(pElementoHijos.children);
-    //console.log(elementosArray);
 
-    for (let x = 0; x < elementosArray.length; x++) {
-        pElementoHijos.removeChild(elementosArray[x]);
-    }
-}
-
-
-function getTablaAmortizacion(pSaldoTotalTabla, pNumDePeriodos, pElementoInicial) {
-
-    let amortizacion = 0;
-    let saldoRestanteTabla = parseFloat(pSaldoTotalTabla);
-    let cuotaTabla = 0;
-    let porcentajeTazaTabla = 2;
-    let interesPesosTabla = 0;
-
-    let fragmentTr = document.createDocumentFragment();
-    let fragmentGroupTd = document.createDocumentFragment();
-
-    let valores = [];
-
-    pNumDePeriodos = rangeAnos.value * 12;
-    amortizacion = myRound(pSaldoTotalTabla / pNumDePeriodos, 10);
-
-    for (let x = 0; x < pNumDePeriodos; x++) {
-
-        let tr = document.createElement('tr')
-
-        fragmentTr.appendChild(tr);
-
-        interesPesosTabla = calcularPorcentajeEnPesos(porcentajeTazaTabla, saldoRestanteTabla);
-
-        cuotaTabla = myRound(interesPesosTabla + amortizacion, 10);
-
-        saldoRestanteTabla = myRound(saldoRestanteTabla, 10) - amortizacion;
-        saldoRestanteTabla = myRound(saldoRestanteTabla, 10);
-
-        valores = [x + 1, saldoRestanteTabla, interesPesosTabla, amortizacion, 0, 0, amortizacion, cuotaTabla];
-
-        for (let i = 0; i < 8; i++) {
-            let td = document.createElement('td');
-            fragmentGroupTd.appendChild(td);
-
-        }
-
-        fragmentGroupTd.children[0].textContent = `${x + 1}`;
-        fragmentGroupTd.children[1].textContent = `$${formatMoneyDecimales(myRound(saldoRestanteTabla))}`;
-        fragmentGroupTd.children[2].textContent = `$${formatMoneyDecimales(myRound(interesPesosTabla))}`;
-        fragmentGroupTd.children[3].textContent = `$${myRound(amortizacion)}`;
-        fragmentGroupTd.children[4].textContent = `$${formatMoneyDecimales(0)}`;
-        fragmentGroupTd.children[5].textContent = `$${formatMoneyDecimales(0)}`;
-        fragmentGroupTd.children[6].textContent = `$${myRound(amortizacion)}`;
-        fragmentGroupTd.children[7].textContent = `$${myRound(cuotaTabla)}`;
-
-        fragmentTr.children[x].appendChild(fragmentGroupTd);
-
-    }
-
-    pElementoInicial.appendChild(fragmentTr);
-
-}
 
 /*
 const getTablaAmortizacionn=(pSaldoTotalTabla, pNumDePeriodos,pElementoInicial)=>{
